7760df360af77169466f937da6ea918b98fbf5f4
Juehang Qin <qinjuehang@hotmail.com>
2025-09-19 11:26:37 -0500

    Refactor profile likelihood functions: move make_profile_llh to _profile.py and remove profile.py

--- src/numerax/stats/__init__.py
+++ src/numerax/stats/__init__.py
@@ -1,6 +1,6 @@
 """Statistics submodule for numerax."""
 
 from . import chi2
-from .profile import make_profile_llh
+from ._profile import make_profile_llh
 
 __all__ = ["chi2", "make_profile_llh"]
